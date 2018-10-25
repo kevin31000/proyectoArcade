10a11b1977b5ef4452fa577e56e88e1aefa51e48
kevin31000 <kevin.arnaiz@opendeusto,es>
2018-10-25 11:42:39 +0200

    primer commit

--- src/proyectoArcade/HelloWorld.java
+++ src/proyectoArcade/HelloWorld.java
@@ -1,11 +1,11 @@
 package proyectoArcade;
 
-public class Prueba {
+public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Prueba");
 
+		System.out.println("Hello world");
 	}
 
 }
