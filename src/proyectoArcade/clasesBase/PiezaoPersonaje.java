6f9e694dabb2cf9ce1da0b282315913fa889c29a
InigoNavarro <33113628+InigoNavarro@users.noreply.github.com>
2018-11-08 11:16:41 +0100

    Clase base para usar para las piezas o los personajes sin imagenes de estos

---
+++ src/proyectoArcade/clasesBase/PiezaoPersonaje.java
@@ -0,0 +1,40 @@
+package proyectoArcade.clasesBase;
+
+public class PiezaoPersonaje {
+
+	int coorX;
+	int coorY;
+	String color;
+	
+	public PiezaoPersonaje(int coorX, int coorY, String color) {
+		super();
+		this.coorX = coorX;
+		this.coorY = coorY;
+		this.color = color;
+	}
+	
+	public int getCoorX() {
+		return coorX;
+	}
+	
+	public void setCoorX(int coorX) {
+		this.coorX = coorX;
+	}
+	
+	public int getCoorY() {
+		return coorY;
+	}
+	
+	public void setCoorY(int coorY) {
+		this.coorY = coorY;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+}
